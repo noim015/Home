99f66cfd58c1587b50df45841b8b1bb13fccf045
sayed imtiaz ahmed <sayed.noim.ahmed@gmail.com>
2015-11-28 22:25:54 +0600

    Bootstrap
    Bootstrap

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+//This code is required for Tooltip and Popover
+$( document ).ready(function() {
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip('show')
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover('show')
+})
+	
+});
